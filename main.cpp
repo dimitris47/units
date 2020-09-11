c2b4ffdd27304aebe4e97e96250672d363e20309
dimitris47 <dimitrisinbox@gmail.com>
2020-09-11 09:17:05 +0300

    fonts

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "global.h"
 #include <QApplication>
+#include <QFont>
 
 int main(int argc, char *argv[]) {
     QLocale::setDefault(QLocale::C);
@@ -8,6 +9,7 @@ int main(int argc, char *argv[]) {
     QApplication::setOrganizationName("DP Software");
     QApplication::setApplicationName(APPNAME);
     QApplication::setApplicationVersion(VERSION);
+    QApplication::setFont(QFont("Fira Sans, Segoe UI, Verdana, Ubuntu, Noto Sans, Arimo, Liberation Sans"));
     MainWindow w;
     w.show();
     return a.exec();
