693ddf12e4ed630202c21755f7de55edce59bcbf
dimitris47 <dimitrisinbox@gmail.com>
2021-07-07 16:22:36 +0300

    add icon attributions & license in sources

--- global.h
+++ global.h
@@ -3,13 +3,18 @@
 
 #include <QString>
 
+const QString br("<br/>");
 const QString APPNAME("Units");
-const QString VERSION("2.0.2");
+const QString VERSION("2.0.3");
 const QString CONTRIBUTORS("Dimitris Psathas");
 
-const QString APPINFO("Program created by Dimitris Psathas<br/><br/>"
-                      "Written in C++, built against the Qt5 toolkit<br/><br/>"
-                      "Published under the MIT License<br/>"
-                      "&copy; 2020");
+const QString APPINFO("Program created by Dimitris Psathas" + br.repeated(2) +
+                      "Written in C++, built against Qt 5.15.2" + br.repeated(2) +
+                      "Published under the MIT License" + br +
+                      "Qt libraries used under (L)GPL3" + br +
+                      "Application icon by <a href='https://icons8.com'>icons8.com</a>" + br +
+                      "In-app icons by <a href='https://icons8.com'>icons8.com</a>" + br +
+                      "and the Ubuntu Yaru theme" + br.repeated(2) +
+                      "&copy; 2020-2021 Dimitris Psathas");
 
 #endif // GLOBAL_H
